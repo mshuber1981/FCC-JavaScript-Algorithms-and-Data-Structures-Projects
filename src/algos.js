5a9669cf1314ac207534c2daa2bf642666d3edb0
Michael Huber <michael.huber1981@outlook.com>
2023-01-05 09:19:07 -0600

    Adding Roman Numerals

--- src/algos.js
+++ src/algos.js
@@ -5,3 +5,38 @@ export function palindromeAlgo(str) {
     str.replace(/[\W_]/g, "").toLowerCase().split("").reverse().join("")
   );
 }
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/roman-numeral-converter
+export function convertToRoman(num) {
+  function getNumeral(digit, lowStr, midStr, nextStr) {
+    switch (true) {
+      case digit <= 3:
+        return lowStr.repeat(digit);
+      case digit === 4:
+        return lowStr + midStr;
+      case digit <= 8: // digits 5-8
+        return midStr + lowStr.repeat(digit - 5);
+      default: // digit 9
+        return lowStr + nextStr;
+    }
+  }
+
+  let str = "";
+
+  // Thousands
+  str += "M".repeat(Math.floor(num / 1000));
+  num %= 1000;
+
+  // Hundreds
+  str += getNumeral(Math.floor(num / 100), "C", "D", "M");
+  num %= 100;
+
+  // Tens
+  str += getNumeral(Math.floor(num / 10), "X", "L", "C");
+  num %= 10;
+
+  // Ones
+  str += getNumeral(num, "I", "V", "X");
+
+  return str;
+}
--- src/pages/RomanNumerals.jsx
+++ src/pages/RomanNumerals.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import { useAppContext } from "../appContext";
+// Algos
+import { convertToRoman } from "../algos";
 // Components
-import { Container } from "react-bootstrap";
+import { Container, Form, Button } from "react-bootstrap";
 import { Title } from "../components/globalStyledComponents";
 
 export default function RomanNumerals() {
+  const [input, setInput] = React.useState("");
+  const [validated, setValidated] = React.useState(false);
+  const [submitted, setSubmit] = React.useState(false);
+  const [inputCopy, setInputCopy] = React.useState("");
+  const [romans, setRomans] = React.useState("");
+  const { theme } = useAppContext();
+
   const pageTitle = "Roman Numeral Converter";
+
+  const handleInputChange = (event) => setInput(event.target.value);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
+    if (form.checkValidity()) {
+      event.preventDefault();
+      setSubmit(true);
+      setInputCopy(input);
+      setRomans(convertToRoman(input));
+      setValidated(false);
+      event.target.reset();
+    }
+  };
 
   React.useEffect(() => {
     const updateTitle = () => (document.title = pageTitle);
@@ -13,13 +42,49 @@ export default function RomanNumerals() {
 
   return (
     <>
-      <section className="section">
+      <section className="section text-center">
         <Container className="d-flex">
           <Title>
             <h2>{pageTitle}</h2>
             <div className="underline"></div>
           </Title>
         </Container>
+        <p className="description">
+          Roman numerals are a numeral system that originated in ancient Rome
+          and remained the usual way of writing numbers throughout Europe well
+          into the Late Middle Ages.
+        </p>
+        <Form
+          className="w-100"
+          noValidate
+          validated={validated}
+          onSubmit={handleSubmit}
+        >
+          <Form.Group>
+            <Form.Control
+              required
+              type="number"
+              placeholder="Enter number to convert"
+              onChange={handleInputChange}
+              className="my-4 mx-auto text-center"
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter at least one number.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Button
+            variant={theme === "light" ? "outline-dark" : "outline-light"}
+            type="submit"
+            className="mb-4"
+          >
+            Submit
+          </Button>
+          {!submitted ? null : submitted ? (
+            <h4 className="text-success">
+              {inputCopy} = {romans}
+            </h4>
+          ) : null}
+        </Form>
       </section>
     </>
   );
