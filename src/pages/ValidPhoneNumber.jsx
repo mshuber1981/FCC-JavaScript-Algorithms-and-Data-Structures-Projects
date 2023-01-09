645f122fc67bd11957f704ac467aba59e3069ab8
Michael Huber <michael.huber1981@outlook.com>
2023-01-09 07:16:37 -0600

    Updating page

--- src/pages/ValidPhoneNumber.jsx
+++ src/pages/ValidPhoneNumber.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+// Algos
+import { telephoneCheck } from "../algos";
 // Components
-import { Container } from "react-bootstrap";
+import { Button, Container, Form } from "react-bootstrap";
 import { Title } from "../components/globalStyledComponents";
 
 export default function ValidPhoneNumber() {
+  const [input, setInput] = React.useState("");
+  const [validated, setValidated] = React.useState(false);
+  const [submitted, setSubmit] = React.useState(false);
+  const [inputCopy, setInputCopy] = React.useState("");
+  const [phoneCheck, setPhoneCheck] = React.useState("");
+
   const pageTitle = "Telephone Number Validator";
+
+  const handleInputChange = (event) => setInput(event.target.value);
+
+  function handleSubmit(event) {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
+    if (form.checkValidity()) {
+      event.preventDefault();
+      setSubmit(true);
+      setInputCopy(input);
+      setPhoneCheck(telephoneCheck(input));
+      setValidated(false);
+      event.target.reset();
+    }
+  }
 
   React.useEffect(() => {
     const updateTitle = () => (document.title = pageTitle);
@@ -20,6 +47,13 @@ export default function ValidPhoneNumber() {
             <div className="underline"></div>
           </Title>
         </Container>
+        <p className="description">
+          The North American Numbering Plan (NANP) is a telephone numbering plan
+          for World Zone 1, which comprises twenty-five distinct regions in
+          twenty countries primarily in North America, including the Caribbean.
+          Some North American countries, most notably Mexico, do not participate
+          in the NANP.
+        </p>
       </section>
     </>
   );
