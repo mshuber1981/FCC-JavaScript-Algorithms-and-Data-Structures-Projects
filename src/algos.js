05654f2c72201e96f2cc26432637c5503f8b890c
Michael Huber <michael.huber1981@outlook.com>
2023-01-17 07:54:04 -0600

    Adding cash register

--- src/algos.js
+++ src/algos.js
@@ -59,3 +59,91 @@ export function telephoneCheck(str) {
   let regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
   return regex.test(str);
 }
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/cash-register
+const denom = [
+  { name: "HUNDREDS", val: 100.0 },
+  { name: "TWENTYS", val: 20.0 },
+  { name: "TENS", val: 10.0 },
+  { name: "FIVES", val: 5.0 },
+  { name: "ONES", val: 1.0 },
+  { name: "QUARTERS", val: 0.25 },
+  { name: "DIMES", val: 0.1 },
+  { name: "NICKELS", val: 0.05 },
+  { name: "PENNYS", val: 0.01 },
+];
+
+export const checkCashRegister = (price, cash, cid) => {
+  let output = { status: null, change: [] };
+  let change = cash - price;
+  // Round change to the nearest hundreth (deals with precision errors)
+  change = Math.round(change * 100) / 100;
+
+  // Transform cid array into drawer object
+  let register = cid.reduce(
+    (acc, curr) => {
+      acc.total += curr[1];
+      acc[curr[0]] = curr[1];
+      return acc;
+    },
+    { total: 0 }
+  );
+
+  // Handle no sale
+  if (change < 0) {
+    output.status = "NO_SALE";
+    output.change = [];
+    return output;
+  }
+
+  // Handle exact change
+  if (change === 0 && register.total !== change) {
+    output.status = "CLOSED";
+    output.change = [];
+    return output;
+  }
+
+  // Handle exact change (nothing left in register)
+  if (register.total === change) {
+    output.status = "CLOSED";
+    output.change = cid;
+    return output;
+  }
+
+  // Handle obvious insufficient funds
+  if (register.total < change) {
+    output.status = "INSUFFICIENT_FUNDS";
+    return output;
+  }
+
+  // Loop through the denomination array
+  let change_arr = denom.reduce((acc, curr) => {
+    let value = 0;
+    // While there is still money of this type in the drawer and the denomination is larger than the change remaining
+    while (register[curr.name] > 0 && change >= curr.val) {
+      change -= curr.val;
+      register[curr.name] -= curr.val;
+      value += curr.val;
+
+      // Round change to the nearest hundreth (deals with precision errors)
+      // change = Math.round(change * 100) / 100;
+    }
+    // Add this denomination to the output
+    if (value > 0) {
+      acc.push([curr.name, value]);
+    }
+    return acc; // Return the current change_arr
+  }, []); // Initial value of empty array for reduce
+
+  // If there are no elements in change_arr or we have leftover change
+  if (change_arr.length < 1 || change > 0) {
+    console.log("this");
+    output.status = "INSUFFICIENT_FUNDS";
+    return output;
+  }
+
+  // Here is your change
+  output.status = "OPEN";
+  output.change = change_arr;
+  return output;
+};
--- src/pages/CashRegister.jsx
+++ src/pages/CashRegister.jsx
@@ -1,10 +1,49 @@
 import React from "react";
+import { useAppContext } from "../appContext";
+// Algos
+import { checkCashRegister } from "../algos";
 // Components
-import { Container } from "react-bootstrap";
+import { Button, Container, Form, Table } from "react-bootstrap";
 import { Title } from "../components/globalStyledComponents";
 
 export default function CashRegister() {
+  const [input, setInput] = React.useState("");
+  const [validated, setValidated] = React.useState(false);
+  const [submitted, setSubmit] = React.useState(false);
+  const [response, setResponse] = React.useState({});
+  const { theme } = useAppContext();
+
   const pageTitle = "Cash Register";
+
+  const register = [
+    ["PENNYS", 1.01],
+    ["NICKELS", 2.05],
+    ["DIMES", 3.1],
+    ["QUARTERS", 4.25],
+    ["ONES", 90],
+    ["FIVES", 55],
+    ["TENS", 20],
+    ["TWENTYS", 60],
+    ["HUNDREDS", 100],
+  ];
+
+  const handleInputChange = (event) => setInput(event.target.value);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
+    if (form.checkValidity()) {
+      event.preventDefault();
+      setSubmit(true);
+      setResponse(checkCashRegister(5.0, input, register));
+      setValidated(false);
+      event.target.reset();
+    }
+  };
 
   React.useEffect(() => {
     const updateTitle = () => (document.title = pageTitle);
@@ -13,13 +52,79 @@ export default function CashRegister() {
 
   return (
     <>
-      <section className="section">
+      <section className="section text-center">
         <Container className="d-flex">
           <Title>
             <h2>{pageTitle}</h2>
             <div className="underline"></div>
           </Title>
         </Container>
+        <Table responsive variant={theme === "light" ? "dark" : "light"}>
+          <thead>
+            <tr>
+              <th>Cash Register</th>
+              {register.map((item, id1) => (
+                <td key={id1}>{item[0]}</td>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Amount</td>
+              {register.map((item, id2) => (
+                <td key={id2}>{`$ ${item[1]}`}</td>
+              ))}
+            </tr>
+          </tbody>
+        </Table>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Form.Group className="my-4">
+            <Form.Label className="h4">
+              What is my change for a $5.00 purchase?
+            </Form.Label>
+            <Form.Control
+              required
+              type="number"
+              step="0.01"
+              placeholder="Enter payment amount"
+              onChange={handleInputChange}
+              className="my-4"
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter a valid amount (example - 10.00).
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Button
+            variant={theme === "light" ? "outline-dark" : "outline-light"}
+            type="submit"
+          >
+            Submit
+          </Button>
+          {!submitted ? null : response.status === "NO_SALE" ? (
+            <h4 className="my-4 text-danger">
+              No sale, you do not have enough money!
+            </h4>
+          ) : submitted &&
+            response.status !== "INSUFFICIENT_FUNDS" &&
+            response.change.length !== 0 ? (
+            <div className="my-4">
+              <h4 className="text-success">Change</h4>
+              <ul className="list-group">
+                {response.change.map((item, id) => (
+                  <li key={id} className="list-group-item">
+                    {item[0]} - ${item[1]}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ) : response.status === "CLOSED" && response.change.length === 0 ? (
+            <h4 className="my-4 text-success">No change due</h4>
+          ) : (
+            <h4 className="my-4 text-danger">
+              No sale, insufficient funds to make change!
+            </h4>
+          )}
+        </Form>
       </section>
     </>
   );
